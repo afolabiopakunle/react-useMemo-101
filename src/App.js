ebb2155f6cbae7efc4efed17942ca4fdcbee7620
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-09 19:47:53 +0100

    commented

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ export default function App() {
 
   const [number, setNumber] = useState(0)
   const [dark, setDark] = useState(false)
-  const doubleNumber = slowFunction();
+  // const doubleNumber = slowFunction();
 
   const themeStyles = {
     backgroundColor: dark ? 'black' : 'white',
     color: dark ? 'white' : 'black'
   }
 
-  function slowFunction(num) {
-    for(let i = 0; i < 10000000000; i++) {}
-    return num * 2
-  }
+  // function slowFunction(num) {
+  //   for(let i = 0; i < 10000000000; i++) {}
+  //   return num * 2
+  // }
 
 
   return (
