c136d96c6038cfc4f4d66409549b45e96f4b56ac
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-09 21:27:07 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import "./style.css";
 
 export default function App() {
 
   const [number, setNumber] = useState(0)
   const [dark, setDark] = useState(false)
-  // const doubleNumber = slowFunction();
+  const doubleNumber = useMemo(() => {
+    return  slowFunction(number);
+  }, [number])
 
   const themeStyles = {
     backgroundColor: dark ? 'black' : 'white',
     color: dark ? 'white' : 'black'
   }
 
-  // function slowFunction(num) {
-  //   for(let i = 0; i < 10000000000; i++) {}
-  //   return num * 2
-  // }
-
+  function slowFunction(num) {
+    for(let i = 0; i < 1000000000; i++) {}
+    return num * 2
+  }
 
   return (
     <>
     <div className="container m-3">
-    <input className='form-control' />
+    <input 
+    value={number} 
+    type='number' 
+    className='form-control w-50'
+    onChange={e => setNumber(parseInt(e.target.value))} />
+
+    <button 
+    className={'btn btn-outline-primary mt-3'}
+    onClick={() => setDark(prevTheme => !prevTheme)}
+    >Change theme</button>
+
+    <div style={themeStyles}>{doubleNumber}</div>
     </div>
     </>
   );
