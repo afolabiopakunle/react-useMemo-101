80f6507c2e44969a7152e25973ec28ab6b5dfb6e
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-09 19:44:33 +0100

    set for bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 export default function App() {
+
+  const [number, setNumber] = useState(0)
+  const [dark, setDark] = useState(false)
+  const doubleNumber = slowFunction();
+
+  const themeStyles = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black'
+  }
+  
+  function slowFunction(num) {
+    for(let i = 0; i < 10000000000; i++) {}
+    return num * 2
+  }
+
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      
+    </>
   );
 }
