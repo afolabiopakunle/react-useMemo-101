7eef459683733870169bd7964b6f143861f302e8
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-09 19:46:05 +0100

    wait reload

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     backgroundColor: dark ? 'black' : 'white',
     color: dark ? 'white' : 'black'
   }
-  
+
   function slowFunction(num) {
     for(let i = 0; i < 10000000000; i++) {}
     return num * 2
@@ -20,7 +20,9 @@ export default function App() {
 
   return (
     <>
-      
+    <div className="container m-3">
+    <input className='form-control' />
+    </div>
     </>
   );
 }
